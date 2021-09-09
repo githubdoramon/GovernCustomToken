225e8cf587176c72419cfd4b0a934a0d823350f3
Samuel Furter <sam@aragon.org>
2021-09-09 15:58:57 +0200

    I've quickly have read the contract and removed the methods which are no longer required

--- contracts/CustomGovernToken.sol
+++ contracts/CustomGovernToken.sol
@@ -13,9 +13,9 @@ import './IERC20.sol';
 // Token to be used on subDAOs for the AN DAO.
 // Changes:
 //   - Rename onlyMinter to onlyController for clarity
-//   - Remove transfer method, so only the controller can transfer ownership (Main DAO, aka community)
+//   - Only the controller can transfer ownership (Main DAO, aka community)
 //   - Allows controller to also burn tokens
-//   - Removes transferWithAuthorization
+//   - No longer required methods removed
 
 contract CustomGovernToken is IERC20, Initializable {
     using SafeMath for uint256;
@@ -24,11 +24,6 @@ contract CustomGovernToken is IERC20, Initializable {
     bytes32 private constant EIP712DOMAIN_HASH = 0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f;
     // bytes32 private constant VERSION_HASH = keccak256("1")
     bytes32 private constant VERSION_HASH = 0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6;
-    // bytes32 public constant PERMIT_TYPEHASH = keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
-    bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
-    // bytes32 public constant TRANSFER_WITH_AUTHORIZATION_TYPEHASH =
-    //     keccak256("TransferWithAuthorization(address from,address to,uint256 value,uint256 validAfter,uint256 validBefore,bytes32 nonce)");
-    bytes32 public constant TRANSFER_WITH_AUTHORIZATION_TYPEHASH = 0x7c7c6cdb67a18743f49ec6fa9b35f50d52ed05cbed4cc592e13b44501c1a2267;
 
     string public name;
     string public symbol;
@@ -37,15 +32,8 @@ contract CustomGovernToken is IERC20, Initializable {
     address public controller;
     uint256 override public totalSupply;
     mapping (address => uint256) override public balanceOf;
-    mapping (address => mapping (address => uint256)) override public allowance;
 
-    // ERC-2612, ERC-3009 state
-    mapping (address => uint256) public nonces;
-    mapping (address => mapping (bytes32 => bool)) public authorizationState;
-
-    event Approval(address indexed owner, address indexed spender, uint256 value);
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event AuthorizationUsed(address indexed authorizer, bytes32 indexed nonce);
     event ChangeController(address indexed controller);
 
     modifier onlyController {
@@ -64,19 +52,6 @@ contract CustomGovernToken is IERC20, Initializable {
         decimals = _decimals;
     }
 
-    function _validateSignedData(address signer, bytes32 encodeData, uint8 v, bytes32 r, bytes32 s) internal view {
-        bytes32 digest = keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                getDomainSeparator(),
-                encodeData
-            )
-        );
-        address recoveredAddress = ecrecover(digest, v, r, s);
-        // Explicitly disallow authorizations for address(0) as ecrecover returns address(0) on malformed messages
-        require(recoveredAddress != address(0) && recoveredAddress == signer, "token: bad sig");
-    }
-
     function _changeController(address newController) internal {
         controller = newController;
         emit ChangeController(newController);
@@ -93,11 +68,6 @@ contract CustomGovernToken is IERC20, Initializable {
         balanceOf[from] = balanceOf[from].sub(value);
         totalSupply = totalSupply.sub(value);
         emit Transfer(from, address(0), value);
-    }
-
-    function _approve(address owner, address spender, uint256 value) private {
-        allowance[owner][spender] = value;
-        emit Approval(owner, spender, value);
     }
 
     function _transfer(address from, address to, uint256 value) private {
@@ -139,34 +109,9 @@ contract CustomGovernToken is IERC20, Initializable {
         return true;
     }
 
-    function approve(address spender, uint256 value) override external returns (bool) {
-        _approve(msg.sender, spender, value);
-        return true;
-    }
-
-    function transfer(address to, uint256 value) override external onlyController returns (bool) {
-        _transfer(msg.sender, to, value);
-        return true;
-    }
-
     function transferFrom(address from, address to, uint256 value) override external onlyController returns (bool) {
-        uint256 fromAllowance = allowance[from][msg.sender];
-        if (fromAllowance != uint256(-1)) {
-            // Allowance is implicitly checked with SafeMath's underflow protection
-            allowance[from][msg.sender] = fromAllowance.sub(value);
-        }
         _transfer(from, to, value);
         return true;
     }
-
-    function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external {
-        require(deadline >= block.timestamp, "token: auth expired");
-
-        bytes32 encodeData = keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline));
-        _validateSignedData(owner, encodeData, v, r, s);
-
-        _approve(owner, spender, value);
-    }
-
 }
 
